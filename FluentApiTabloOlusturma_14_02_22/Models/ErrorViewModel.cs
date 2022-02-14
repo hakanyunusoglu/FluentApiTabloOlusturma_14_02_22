d3f7a232fbccdad8d11ed673a634d3b29186a6e6
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-02-14 10:33:58 +0300

    Add project files.

---
+++ FluentApiTabloOlusturma_14_02_22/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FluentApiTabloOlusturma_14_02_22.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FluentApiTabloOlusturma_14_02_22/Models/FluentApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FluentApiTabloOlusturma_14_02_22.Models
+{
+    public class FluentApiContext: DbContext
+    {
+        public FluentApiContext(DbContextOptions<FluentApiContext> options) : base(options)
+        {
+
+        }
+    }
+}
